5fc4e54036d19148d3a40c447a0c9fae98c359a3
yo aimo <etvi2004@gmail.com>
2023-10-24 13:48:01 +0300

    Created db

---
+++ DataBase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeautyArt
+{
+    internal class DataBase
+    {
+        static string con = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=BeautyArt;Integrated Security=True;Connect Timeout=30;";
+        SqlConnection sqlConnection = new SqlConnection(con);
+
+        public void Open()
+        {
+            if (sqlConnection.State == ConnectionState.Open)
+            {
+                sqlConnection.Close();
+            }
+            else if (sqlConnection.State == ConnectionState.Closed)
+            {
+                sqlConnection.Open();
+            }
+        }
+
+    }
+}
